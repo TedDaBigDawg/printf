f32ebc9b32fcad2ee53d592cc4548d725e8f32eb
Ted Osiobe <osiobeted@gmail.com>
2022-10-23 10:08:03 -0700

    printf

--- precision.c
+++ precision.c
@@ -11,28 +11,24 @@
 
 char *get_precision(char *s, params_t *params, va_list ap)
 {
-	int i = 0;
+	int d = 0;
 
 	if (*s != '.')
-	{
 		return (s);
-	}
-
 	s++;
-
 	if (*s == '*')
 	{
-		i = va_arg(ap, int);
+		d = va_arg(ap, int);
 		s++;
 	}
 	else
 	{
 		while (_isdigit(*s))
 		{
-			i = i * 10 + (*s++ - '0');
+			d = d * 10 + (*s++ - '0');
 		}
 	}
-	params->precision = i;
+	params->precision = d;
 
 	return (s);
 }
